b4cc6d1f5d3d2713954eae6c937b380b9d0c2062
LlucVelazquez <lluc.velazquez.7e8@itb.cat>
2024-12-05 11:40:56 +0100

    Add project files.

---
+++ t2-pp2-examen-LlucVelazquez/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace t2_pp2_examen_LlucVelazquez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
